e49e2fcc424bae0f075b29d74fd41120e9d3dfa3
LOIC.AGST <loic.agst@eduge.ch>
2023-08-22 11:29:36 +0200

    Mise à jour du jeu de Nim

--- S1/E5.py
+++ S1/E5.py
@@ -1,11 +1,21 @@
-def calcul_pieces():
-    pass
+NB_MAX_PIECES = 100
+
+
+def calcul_pieces(prix_piece, nombre_pieces, rabais=10/100):
+    if nombre_pieces > NB_MAX_PIECES:
+        prix_piece = prix_piece * rabais
+    elif nombre_pieces >= NB_MAX_PIECES:
+        prix_piece = prix_piece
+    return prix_piece
 
 
 def main():
     prix_unitaire = int(input("Entrer un prix unitaire : "))
     nb_pieces = int(input("Entrer un nombre de pieces : "))
-    print(f"Pour {nb_pieces} pièces à {prix_unitaire}€ : \n - Prix avant rabais : {prix_unitaire * nb_pieces}€ \n - Prix après rabais : {prix_unitaire * nb_pieces} \n - Montant du rabais : ")
+    print(f"Pour {nb_pieces} pièces, le prix unitaire est de {prix_unitaire}€")
+    print(f"- Prix avant rabais : {prix_unitaire} * {nb_pieces}€")
+    print(f"- Montant du rabais : {rabais * 100}%")
+    print(calcul_pieces(prix_unitaire, nb_pieces, rabais=10/100))
 
 
 if __name__ == '__main__':
--- S1/game-allumette.py
+++ S1/game-allumette.py
@@ -1,16 +1,33 @@
+import turtle as np
+import random
+
+
 def game_allumette():
-    allu_depart = int(input("Joueur 1 prends : "))
-    if allu_depart > 3:
-        print("Le nombre max est 3 allumettes !")
+    nom_joueurs = ["Joueur 1", "Ordinateur 1"]  # Liste des joueurs
+    depart_joueurs = random.choice(nom_joueurs)  # Choisir un joueur au hasard
+
+    allu_joueur1 = 0
+    allu_ordi = 0
+
+    if depart_joueurs == nom_joueurs[0]:
+        allu_joueur1 = int(
+            input("Joueur 1 : Combien d'allumettes voulez-vous prendre ? "))
+        if allu_joueur1 > 3:
+            print("Vous ne pouvez pas prendre plus de 3 allumettes !")
+        print(f"Joueur 1 : A pris {allu_joueur1} allumettes")
+    elif depart_joueurs == nom_joueurs[1]:
+        allu_ordi = random.randint(1, 3)
+        print(f"Ordinateur 1 : A pris {allu_ordi} allumettes")
+        allu_joueur1 = int(
+            input("Joueur 1 : Combien d'allumettes voulez-vous prendre ? "))
+        if allu_joueur1 > 3:
+            print("Vous ne pouvez pas prendre plus de 3 allumettes !")
+        print(f"Joueur 1 : A pris {allu_joueur1} allumettes")
+    allu_total = allu_joueur1 - allu_ordi
+    if allu_total < 4:
+        print("Ordi 1 : A gagné !")
     else:
-        print(f"Joueur 1 : A pris {allu_depart} allumettes")
-        allu_fin = 4 - allu_depart
-        print(f"Ordinateur 1 : A pris {allu_fin} allumettes")
-        allu_total = allu_depart - allu_fin
-        if allu_total < 4:
-            print("Ordi 1 : A gagné !")
-        else:
-            print("Ordi 1 : A perdu !")
+        print("Ordi 1 : A perdu !")
 
 
 def main():
