965571ab3f48a0acfddb2b3dbf33ad5d5e8df2b0
MrsWmps <loic.agst@eduge.ch>
2023-08-29 09:43:49 +0200

    Mise à jour du fichier "game-allumette.py"

---
+++ S1/anneeBissextile.py
@@ -0,0 +1,53 @@
+# # initialisation des variables
+# annee=2015
+# bissextile=False
+
+# # instructions conditionnelles SANS opérateurs booléans
+# if annee % 4 == 0:
+#     bissextile = True
+# else:
+#     if annee % 100 == 0:
+#         bissextile = True
+#     else:
+#         if annee % 400 == 0:
+#             bissextile = True
+#         else:
+#             bissextile = False
+# if bissextile == True:
+#     print('Bissextile')
+# else:
+#     print('Non bissextile')
+
+"""" 
+Savoir par un autre algorithme si une année est bissextile ou non
+"""
+
+def bissextile(annee, diviseur):
+    while diviseur == 0:
+        print("Le diviseur ne peut pas être nul ! \n")
+        diviseur = int(input('Entrez un autre diviseur disponible (4, 100, 400) : \n'))
+
+    if diviseur == 4:
+        bissextile = annee % diviseur == 0
+    elif diviseur == 100:
+        bissextile = annee % diviseur == 0
+    elif diviseur == 400:
+        bissextile = annee % diviseur == 0
+    else:
+        pass
+        return
+
+    if bissextile:
+        print("L'année est bissextile")
+    else:
+        print("L'année n'est pas bissextile")
+
+
+def main():
+    annee = int(input('Entrez une année : '))
+    diviseur = int(input('Entrez un diviseur (4, 100, 400) : '))
+
+    bissextile(annee, diviseur)
+
+if __name__ == '__main__':
+    main()
--- S1/game-allumette-v2.py
+++ S1/game-allumette-v2.py
@@ -17,55 +17,34 @@ def game_allumette():
     while allu_total > 0:
 
         def affichage_allumettes(allumettes):
-            tab_joueur = []  # On initialise une liste pour stocker les allumettes
-
-            for i in range(allumettes):
-                # On ajoute un "I" à chaque tour de boucle
-                tab_joueur.append("I")
-            # On affiche le tableau d'allumettes séparées par un espace
+            tab_joueur = ["I" for _ in range(allumettes)]
+            
             if tab_joueur == []:
                 print("Il n'y a plus d'allumettes !")
             else:
                 print(f"Allumettes restantes : {np.array(tab_joueur)}")
 
         if depart_joueurs == nom_joueurs[0]:
-
-            # On initialise la variable à 0 pour entrer dans la boucle
             allu_joueur1 = 0
 
-            # On vérifie que le nombre d'allumettes prises est bien entre 1 et 3
-            while allu_joueur1 < 1 or allu_joueur1 > 3:
-                allu_joueur1 = int(
-                    input("Joueur 1 : Combien d'allumettes voulez-vous prendre (entre 1 et 3) ? "))
-                if allu_joueur1 < 1 or allu_joueur1 > 3:
-                    print("Vous ne pouvez prendre que 1, 2 ou 3 allumettes !")
+            while allu_joueur1 < 1 or allu_joueur1 > min(3, allu_total):
+                allu_joueur1 = int(input("Joueur 1 : Combien d'allumettes voulez-vous prendre (entre 1 et 3) ? "))
+                
+                if allu_joueur1 < 1 or allu_joueur1 > min(3, allu_total):
+                    print(f"Vous ne pouvez prendre que 1, 2 ou 3 allumettes (maximum {min(3, allu_total)}) !")
 
-                if allu_joueur1 > allu_total:
-                    print(
-                        f"Vous ne pouvez pas prendre plus que ce qu'il reste ({allu_total} allumettes) !")
             allu_total -= allu_joueur1
-
-            # On actualise la liste d'allumettes pour le joueur suivant
             affichage_allumettes(allu_total)
-
             print(f"Joueur 1 : A pris {allu_joueur1} allumettes")
 
         elif depart_joueurs == nom_joueurs[1]:
-            """
-            Éviter de prendre plus que ce qu'il reste
-            On initialise la variable pour entrer dans la boucle
-            """
             allu_ordi = random.randint(1, min(3, allu_total))
             allu_total -= allu_ordi
             print(f"Ordinateur 1 : A pris {allu_ordi} allumettes")
 
-        # On affiche le nombre d'allumettes restantes pour les deux joueurs
         print(f"Il reste {allu_total} allumettes.")
-
-        # On actualise la liste d'allumettes pour le joueur suivant
         affichage_allumettes(allu_total)
 
-        # Changer de joueur
         depart_joueurs = nom_joueurs[1] if depart_joueurs == nom_joueurs[0] else nom_joueurs[0]
 
     if depart_joueurs == nom_joueurs[0]:
