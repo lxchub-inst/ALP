cfd40bb5f9c38197902d03de551c2cbd079917b8
MrsWmps <loic.agst@eduge.ch>
2023-08-30 11:33:20 +0200

    Update ailes.py

--- S1/ailes.py
+++ S1/ailes.py
@@ -5,14 +5,31 @@
 # De plus, si le voyageur a déjà volé avec la compagnie lors de l'année courante, il bénéficie d'un rabais supplémentaire (sur le prix de base) 
 # proportionnel au nombre de vols effectués; celui ci se monte à 10% pour un vol, à 15% pour 2, 3, ou 4 vols et à 20% pour 5 vols ou plus.
 
-def calcul():
-    pass
+def calcul(prix_initial, type_voyageur, nombre_vol):
+    rabaisSup = 0
+    rabais = 0
+    if type_voyageur == 1:
+        rabais = (prix_initial * 20/100)
+    elif type_voyageur == 2:
+        rabais = (prix_initial * 10/100)
+    
+    if nombre_vol == 1:
+        rabaisSup = (prix_initial * 10/100)
+    elif nombre_vol >= 2 and nombre_vol <= 4:
+        rabaisSup = (prix_initial * 15/100)
+    else:
+        rabaisSup = (prix_initial * 20/100)
+    
+    prix_initial = prix_initial - rabais - rabaisSup
+    return prix_initial
+
 
 def main():
     prix_initial = int(input("Prix initial : "))
-    type_voyageur = input("Type de voyageur : ")
+    type_voyageur = int(input("Type de voyageur : "))
     nombre_vol = int(input("Nombre de vols : "))
-    calcul()
+
+    print(f"Prix effectif : {calcul(prix_initial, type_voyageur, nombre_vol)}")
 
 if __name__ == "__main__":
     main()
