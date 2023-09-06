f9a0d3e65531e3698f76dfafe8973833a3050464
MrsWmps <loic.agst@eduge.ch>
2023-09-06 09:54:02 +0200

    Update Jeu_de_Nim_v2.py

--- S1/Jeu_de_Nim_v2.py
+++ S1/Jeu_de_Nim_v2.py
@@ -51,24 +51,22 @@ def choix_joueurs():
             allu_joueur1 = 0
             while allu_joueur1 < 1 or allu_joueur1 > min(3, allu_total):
                 allu_joueur1 = int(
-                    input("Joueur 1 : Combien d'allumettes voulez-vous prendre (entre 1 et 3) ? "))
+                    input("Combien d'allumettes voulez-vous prendre (entre 1 et 3) ? "))
 
                 if allu_joueur1 < 1 or allu_joueur1 > min(3, allu_total):
-                    print(
-                        f"Vous ne pouvez prendre que 1, 2 ou 3 allumettes (maximum {min(3, allu_total)}) !")
-                    """ 
-                        Si le joueur entre un nombre d'allumettes incorrect, on lui affiche un message d'erreur et on lui redemande de saisir un nombre d'allumettes
-                        """
+                    print(f"Vous ne pouvez prendre que 1, 2 ou 3 allumettes (maximum {min(3, allu_total)}) !")¨
+                    
+                    """ Si le joueur entre un nombre d'allumettes incorrect, on lui affiche un message d'erreur et on lui redemande de saisir un nombre d'allumettes"""
 
             # On retire le nombre d'allumettes que le joueur a pris au nombre d'allumettes restantes
             allu_total -= allu_joueur1
-            print(f"Joueur 1 : A pris {allu_joueur1} allumettes")
+            print(f"Vous avez pris {allu_joueur1} allumettes")
 
         elif depart_joueurs == nom_joueurs[1]:
             # L'ordinateur prend un nombre d'allumettes aléatoire entre 1 et 3 (ou le nombre d'allumettes restantes si il en reste moins de 3)
             allu_ordi = random.randint(1, min(3, allu_total))
             allu_total -= allu_ordi
-            print(f"Ordinateur 1 : A pris {allu_ordi} allumettes")
+            print(f"Ordinateur a pris {allu_ordi} allumettes")
 
         affichage_allumettes(allu_total)
         depart_joueurs = changement_joueurs(depart_joueurs, nom_joueurs)
