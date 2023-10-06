edecfd71326ad806de1f6f17a31d49b19f5723e3
MrsWmps <loic.agst@eduge.ch>
2023-10-06 11:48:43 +0200

    Update mcdo_squelette_liste.py

--- mcdo_squelette_liste.py
+++ mcdo_squelette_liste.py
@@ -10,61 +10,106 @@ MAINS_CAL = [498, 257, 504, 393]
 SIDES_CAL = [435, 389 ,90]
 DRINKS_CAL = [175, 1, 0, 80]
 
+# Constantes pour le surplus
+SURPLUS_CAL = 0.25
+
 #Exercice 2 écrivez une fonction qui affiche le contenu de la liste passée en paramètre (ze_liste)
 def show_list_items(ze_list):
-   pass
+    for i in range(len(ze_list)):
+        print(ze_list[i])
+    return None
 
 #Exercice 3 écrivez une fonction qui retourne l'index de la valeur la plus grande
 #de la liste passée en paramètre (ze_liste)
 def get_index_of_max(ze_list):
-    pass 
+    max_index = 0
+    for i in range(1, len(ze_list)):
+        if ze_list[i] > ze_list[max_index]:
+            max_index = i
+    return max_index
 
-#Exercice 3 écrivez une fonction qui retourne l'index de la valeur la plus plus petite
-#de la liste passée en paramètre (ze_liste)
 def get_index_of_min(ze_list):
-    pass 
+    min_index = 0
+    for i in range(1, len(ze_list)):
+        if ze_list[i] < ze_list[min_index]:
+            min_index = i
+    return min_index
+
 
 #Exercice 3 écrivez une fonction qui affiche le choix le plus calorique et le moins calorique
 #selon les listes, cette fonction APPELLE les fonctions min et max !
 def show_max_min_choice(cal_list, dish_list):
-     pass 
+    max_calories = get_index_of_max(cal_list)
+    min_calories = get_index_of_min(cal_list)
+
+    print("Le choix le plus calorique est", dish_list[max_calories])
+    print("Le choix le moins calorique est", dish_list[min_calories])
 
 def get_user_choices():
     # Demande à l'utilisateur de choisir un plat principal
     print("\nChoisissez un plat :")
-    #show_list_items(MAINS) #décommentez appel de la fonction de l'exercice 2
+    show_list_items(MAINS) #décommentez appel de la fonction de l'exercice 2
     #ajouter partie exercice 3
+    show_max_min_choice(MAINS_CAL, MAINS)
     main_choice = int(input("Entrez le numéro correspondant : "))
 
     # Demande à l'utilisateur de choisir un accompagnement
     print("\nChoisissez un accompagnement :")
-    #show_list_items(SIDES) #décommentez appel de la fonction de l'exercice 2
+    show_list_items(SIDES) #décommentez appel de la fonction de l'exercice 2
     #ajouter partie exercice 3
+    show_max_min_choice(SIDES_CAL, SIDES)
     side_choice = int(input("Entrez le numéro correspondant : "))
 
     # Demande à l'utilisateur de choisir une boisson
     print("\nChoisissez une boisson :")
-    #show_list_items(DRINKS) #décommentez appel de la fonction de l'exercice 2
+    show_list_items(DRINKS) #décommentez appel de la fonction de l'exercice 2
     #ajouter partie exercice 3
+    show_max_min_choice(DRINKS_CAL, DRINKS)
     drink_choice = int(input("Entrez le numéro correspondant : "))
-            
+
     return main_choice, side_choice, drink_choice #retour des valeurs saisies
 
 
 def get_tot_calories(main_course, side, drink):
     # Exercice 4 : Calcul des calories en fonction des choix de l'utilisateur sans effet papillon
+    return MAINS_CAL[main_course - 1] + SIDES_CAL[side - 1] + DRINKS_CAL[drink - 1]
+
+
+def get_tot_calories_with_butterfly(main_choice, side_choice, drink_choice):
+    surplus = 0
+    main_choice_calories = get_index_of_max(MAINS_CAL)
+    side_choice_calories = get_index_of_max(SIDES_CAL)
+    original_calories = MAINS_CAL[main_choice - 1] + SIDES_CAL[side_choice - 1] + DRINKS_CAL[drink_choice - 1]
+
+    # Vérifiez si l'utilisateur a choisi les options les plus caloriques pour le plat principal et l'accompagnement
+    if (main_choice - 1 == main_choice_calories and side_choice - 1 == side_choice_calories):
+        surplus = original_calories + (original_calories * SURPLUS_CAL)
+    else:
+        surplus = original_calories
+
+    return surplus
+
+
+def get_item_under_300_calories(main_course, side, drink):
     pass
-   
 
 # Procédure main()
 def main():
-    # main_choice, side_choice, drink_choice = get_user_choices()
-    
+    main_choice, side_choice, drink_choice = get_user_choices()
+
     #Exercice 4 appel de la fonction get_tot_calories
-    
+    total_calories = get_tot_calories(main_choice, side_choice, drink_choice)
+    total_calories_with_butterfly = get_tot_calories_with_butterfly(main_choice, side_choice, drink_choice)
+
     #Exercice 4 affichage du résultat
-    pass
-    
+    print("\n Calories total de votre menu", total_calories, "kcal")
+    print("\n Calories total de votre menu", total_calories_with_butterfly, "kcal")
+
+    print(get_index_of_max(MAINS_CAL))
+    print(get_index_of_max(SIDES_CAL))
+
+
+
 # Appel de la procédure main()
 if __name__ == "__main__":
     main()
