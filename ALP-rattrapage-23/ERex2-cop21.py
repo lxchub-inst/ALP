3a92db06624f838d8cfa8ca92db674e72dbd0a69
MrsWmps <loic.agst@eduge.ch>
2023-11-17 11:48:49 +0100

    Update ERex2-cop21.py

--- ALP-rattrapage-23/ERex2-cop21.py
+++ ALP-rattrapage-23/ERex2-cop21.py
@@ -7,22 +7,44 @@
 
 
 def corriger_donnees(lst_pourcentages):
-    pass
+    for elements in range(len(lst_pourcentages)):
+        if lst_pourcentages[elements] < 0 or lst_pourcentages[elements] > 100:
+            lst_pourcentages[elements] = 0
+        if lst_pourcentages[elements] < 1:
+            lst_pourcentages[elements] *= 100
+    return lst_pourcentages
 
 
 ''' Cette fonction retourne la moyenne des valeurs de la liste des pourcentages reçue en 
 paramètre sans prendre en compte les valeurs nulles. '''
 
 
+def elements(lst_pourcentages):
+    somme_element = 0
+    for element in range(len(lst_pourcentages)):
+        if lst_pourcentages[element] != 0:
+            somme_element += lst_pourcentages[element]
+    return somme_element
+
+
 def moyenne_sans_0(lst_pourcentages):
-    ''' A CODER '''
+    return elements(lst_pourcentages) / 6
 
 
 ''' Cette procédure affiche le pays ayant le pourcentage le plus proche du pourcentage recherché '''
 
 
 def afficher_pays_le_plus_proche(lst_pays, lst_pourcentages, pourcentage_recherche):
-    pass
+    ele_pays = 0
+    ele_pourcentage = 0
+    for ele_pourcentages in range(len(lst_pourcentages)):
+        if lst_pourcentages[ele_pourcentages] != 0:
+            # GPT pour cette ligne
+            if abs(lst_pourcentages[ele_pourcentages] - pourcentage_recherche) < abs(ele_pourcentage -
+                                                                                     pourcentage_recherche):
+                ele_pourcentage = lst_pourcentages[ele_pourcentages]
+                ele_pays = lst_pays[ele_pourcentages]
+    print("Le pays le plus proche est", ele_pays, "avec", ele_pourcentage, "%")
 
 
 ''' Cette procédure affiche les nb premières valeurs non nulles de la liste des pourcentages, 
@@ -30,7 +52,17 @@ puis indique le nombre de valeurs affichées. '''
 
 
 def afficher_nb_donnees(lst_pourcentages, nb):
-    ''' A CODER '''
+    nb_valeur = 0
+    # GPT pour cette ligne
+    lst_valeurs = []
+    for ELEMENTS in range(len(lst_pourcentages)):
+        if lst_pourcentages[ELEMENTS] != 0:
+            # GPT pour cette ligne
+            lst_valeurs.append(round(lst_pourcentages[ELEMENTS]))
+            nb_valeur += 1
+        if nb_valeur == nb:
+            break
+    print("Les", nb_valeur, "valeurs de pourcentages affichés sont", lst_valeurs)
 
 
 # Procédure main()
