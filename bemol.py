26b7e6428c68243d9e97f523b6f971358434c28a
MrsWmps <loic.agst@eduge.ch>
2023-12-13 08:05:41 +0100

    Update bemol.py

--- bemol.py
+++ bemol.py
@@ -5,37 +5,46 @@ TYPE_BEMOL = "b"
 TYPE_DIESE = "#"
 
 
-def bemol(nb_notes):
+def recap_notes(type_notes, nb_notes):
+
+    dico_notes = {"do": "C", "ré": "D", "mi": "E", "fa": "F", "sol": "C", "la": "A", "si": "B"}
+
+    for i in range(len(liste_note)):
+        gamme_notes = altertaions_gamme(type_notes, nb_notes)
+        print("La gamme avec", i+1, type_notes, "est gamme", gamme_notes, "en anglais", dico_notes[gamme_notes])
+
+
+def diese(nb_notes):
     ele_notes = 0
     note_bemol = ""
 
     while nb_notes > 0:
-        if ele_notes + SAUT_NOTE < len(liste_note):
+        if (ele_notes + SAUT_NOTE) < len(liste_note):
             ele_notes = ele_notes + SAUT_NOTE
         else:
             ele_notes = ele_notes + SAUT_NOTE - len(liste_note)
 
         note_bemol = liste_note[ele_notes]
-        nb_notes -= 1
+        nb_notes = nb_notes - 1
     return note_bemol
 
-def diese(nb_notes):
+def bemol(nb_notes):
     ele_notes = 0
     notes_diese = ""
 
     while nb_notes > 0:
-        if ele_notes - SAUT_NOTE >= 0:
-            ele_notes = ele_notes + SAUT_NOTE
+        if (ele_notes - SAUT_NOTE) >= 0:
+            ele_notes = ele_notes - SAUT_NOTE
         else:
             ele_notes = len(liste_note) + ele_notes - SAUT_NOTE
 
         notes_diese = liste_note[ele_notes]
-        nb_notes -= 1
-    return nb_notes
+        nb_notes = nb_notes - 1
+    return notes_diese
 
 
 def altertaions_gamme(type_notes, nb_notes):
-    print("Les notes altérées osnt : ", end=" ")
+    print("Les notes altérées sont : ", end=" ")
     notes_debut = 0
     notes_fin = nb_notes
     notes_etapes = 1
@@ -64,6 +73,11 @@ def main():
     print("Pour les", nb_notes, "altérartions de type", type_notes, ", la gamme de base est",
           gamme_note)
     altertaions_gamme(type_notes, nb_notes)
+    print("\n Récpap de #")
+    print(recap_notes(type_notes, nb_notes))
+    print("\n")
+    print("Récap de b")
+    print(recap_notes(type_notes, nb_notes))
 
 
 main()
